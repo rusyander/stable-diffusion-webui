de1ac6da79965acaded40791802869940a19bb7f
rustam <rustam.urmanov.1996@gmail.com>
2026-01-18 20:50:51 +0500

    up

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,9 @@
 @echo off
 
-set PYTHON=
+set PYTHON=C:\Python310\python.exe
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--api --xformers --opt-sdp-attention --no-half-vae --skip-python-version-check --skip-torch-cuda-test
+set STABLE_DIFFUSION_COMMIT_HASH=
 
 call webui.bat
